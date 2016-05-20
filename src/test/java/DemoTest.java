524a4cb5c451e5c5fe0db9f1bdbba9332fe9e3c5
priyankpatel10 <priyank1410@gmail.com>
2016-05-20 14:39:55 +0530

    gradle commit

---
+++ src/test/java/DemoTest.java
@@ -0,0 +1,15 @@
+package tester;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class DemoTest{
+
+	@Test
+	public void test(){
+	assertTrue(true);
+
+}
+}
